f375350b6e2116d1aa387f5fb9a5a42c895e9d6a
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2022-08-30 11:27:36 -0400

    deploy to pages

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function greet() {
+	const currYear = new Date().getFullYear();
+
+	const getName = prompt("What is your name?");
+	alert(`Hello, ${getName}`);
+
+	const getAge = prompt("How old are you?");
+	const age = parseInt(getAge);
+
+	const confBday = confirm("Have you had a birthday yet?");
+	const thisYear = currYear - age;
+	const prevYear = currYear - (age + 1);
+
+	if (confBday) {
+		alert(`You were born in ${thisYear}`);
+	} else if (!confBday) {
+		alert(`You were born in ${prevYear}`);
+	}
+}
